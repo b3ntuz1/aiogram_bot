7eb6a277c1e56322ece92a231bef91e147bda2c7
tony <tonibalog@gmail.com>
2021-01-12 17:55:43 +0200

    Procfile

--- bot.py
+++ bot.py
@@ -7,6 +7,15 @@ from config import TOKEN
 bot = Bot(token=TOKEN)
 dp = Dispacher(bot)
 
+# webhook settings
+WEBHOOK_HOST = 'https://aiogram-bot.heroku.com'
+WEBHOOK_PATH = '/'
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
+# webserver settings
+WEBAPP_HOST = 'localhost'  # or ip
+WEBAPP_PORT = 3001
+
 @dp.message_handler(command="start")
 async def start_command(message: types.Message):
 	await message.reply("Hello")
@@ -20,4 +29,6 @@ async def ping_command(message: types.Message):
 	await message.reply("ping")
 
 if __name__ == "__main__":
-	executor.start_polling(dp)
+	# executor.start_polling(dp)
+	start_webhook(dispatcher=dp, webhook_path=WEBHOOK_PATH, on_startup=on_startup, on_shutdown=on_shutdown,
+                  skip_updates=True, host=WEBAPP_HOST, port=WEBAPP_PORT)

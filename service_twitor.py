b71919000720f18abc310775970bd2d98a59f262
Tony <tonibalog@gmail.com>
2021-06-11 02:12:55 +0300

    Minor text fixes

--- service_twitor.py
+++ service_twitor.py
@@ -68,7 +68,7 @@ class Twitor:
                     post_status = users[user]
                     urlToTweet = str(self.tw_url + tweet["id_str"])
                     text = f'[{tweet["user"]["name"]}]({urlToTweet}) tweeted:\n'
-                    text += tweet.text + "\n"
+                    text += tweet.get('text') + "\n"
                     tweets.append([text, post_status, user])
         self.save_twid(self.twid)
         return tweets
